a5a68a05bb25cd97dd955ceea3fbf080bec74420
pieR <p.Soriot@rubika-edu.com>
2019-10-11 18:35:25 +0200

    Tentative d'ui 1.0

---
+++ Unity/Assets/Scripts/Sc_Chara_Interface.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Sc_Chara_Interface : MonoBehaviour
+{
+    [Header("Cosmetic")]
+    [SerializeField] TextMeshProUGUI charaName;
+    [SerializeField] Image charaFace;
+    [SerializeField] TextMeshProUGUI charaOccupation;
+
+    [Header("Life")]
+    [SerializeField] TextMeshProUGUI charaState;
+    [SerializeField] Animator charaStateAnim;
+
+    [Header("Skills")]
+    [SerializeField] Image[] oneSkillSlot;
+    [SerializeField] Image[] twoSkillSlot;
+    [SerializeField] Image[] threeSkillSlot;
+    [SerializeField] Image[] fourSkillSlot;
+
+    [Header("Equipment")]
+    [SerializeField] Image[] itemSlot;
+
+}
