5a409a86a8d49e50d0d8c3b29845f96d7c0108d2
Quentin Denois <Quentin.denois@hotmail.fr>
2019-10-29 01:19:11 +0100

    AI Random Movement to improve

--- Unity/Assets/Scripts/Sc_IACharacter.cs
+++ Unity/Assets/Scripts/Sc_IACharacter.cs
@@ -1,9 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+public enum State
+{
+    Free,
+    Captured
+}
 
 public class Sc_IACharacter : MonoBehaviour
 {
+
+    public NavMeshAgent agent;
+    public float trialsAreaSize = 5f;
+
+    public Tile IATile;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +27,10 @@ public class Sc_IACharacter : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void RandomMovement()
+    {
+        agent.SetDestination(transform.position + new Vector3(Random.Range(1, trialsAreaSize), 0f, Random.Range(1, trialsAreaSize)));
     }
 }
